315f15206c0ec9069343021a0b669b3852d436b8
lucaidau <huydarealest@gmail.com>
2021-06-04 10:16:17 +0700

    sub

--- main.cpp
+++ main.cpp
@@ -2,15 +2,30 @@
 #include "cmath"
 using std::cout;
 using std::cin;
-bool isPrime(int n) {
+/*bool isPrime(int n) {
     for (int i = 2; i < n; i++)
         if (n % i == 0) {
             // n chia hết cho số khác 1 và chính nó.
             return false;
         }
     return n > 1;
+}*/
+void sieve(int N) {
+    bool isPrime[N+1];
+    for(int i = 0; i <= N;++i) {
+        isPrime[i] = true;
+    }
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for(int i = 2; i * i <= N; ++i) {
+        if(isPrime[i] == true) {
+            // Mark all the multiples of i as composite numbers
+            for(int j = i * i; j <= N; j += i)
+                isPrime[j] = false;
+        }
+    }
 }
-bool isPrime(int n) {
+bool isPrime1(int n) {
     for (int i = 2; i <= sqrt(n); i++)
         if (n % i == 0) return false;//ko la so ng to
     return n > 1;
@@ -18,9 +33,6 @@ bool isPrime(int n) {
 int main() {
     int n;
     cin>>n;
-    if(isPrime(n)==0)
-        cout<<"khong la so ng to";
-    else
-        cout<<"la so ng to";
+    sieve(n);
     return 0;
 }
